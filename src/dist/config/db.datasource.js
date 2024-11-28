d716b0cfa14b605615e0d8da3ce583e8fb13ae50
divinomarciok <58217165+divinomarciok@users.noreply.github.com>
2024-11-28 11:31:09 -0300

    Alteraçao no vercel.json

--- src/dist/config/db.datasource.js
+++ src/dist/config/db.datasource.js
@@ -11,15 +11,15 @@ const dotenv_1 = __importDefault(require("dotenv"));
 // Carregar as variáveis de ambiente do arquivo .env
 dotenv_1.default.config();
 exports.AppDataSource = new typeorm_1.DataSource({
-    type: 'postgres', // Tipo do banco de dados
-    host: process.env.DB_HOST || 'localhost', // Host do banco
-    port: parseInt(process.env.DB_PORT || '5432', 10), // Porta do banco
-    username: process.env.DB_USERNAME || 'user', // Usuário do banco
-    password: process.env.DB_PASSWORD || 'password', // Senha do banco
-    database: process.env.DB_DATABASE || 'meu_banco', // Nome do banco de dados
-    synchronize: true, // Cuidado: Não use em produção
-    logging: true, // Log de SQL (útil para debug)
-    entities: [user_1.user], // Definição das entidades
-    migrations: [], // Migrations, caso você tenha
-    subscribers: [], // Caso você tenha subscribers
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME || 'user',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_DATABASE || 'meu_banco',
+    synchronize: true,
+    logging: true,
+    entities: [user_1.user],
+    migrations: ["src/migrations/*.ts"],
+    subscribers: [],
 });
