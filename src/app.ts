29f91bb731ed63fa087fc86dfd891d281a39297e
divinomarciok <58217165+divinomarciok@users.noreply.github.com>
2024-11-29 23:15:27 -0300

    Correcao das migrations

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,35 @@
 import express from "express";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 import router from "./routes/routes";
 import { AppDataSource } from "./config/db.datasource";
 import cors from "cors";
 
-dotenv.config();
-
+//dotenv.config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`[LOG] ${req.method} ${req.url}`);
+  next();
+});
+
+app.use("/", router);
 
 app.get("/", (req, res) => {
   res.send("API funcionando!");
 });
 
-app.use("/",router);
 
-console.log("Aplicação funcionando....");
 
 AppDataSource.initialize()
-.then(() =>{
-  console.log("Banco de dados conectado");
-  const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  .then(() => {
+    console.log("Banco de dados conectado");
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar no banco de dados:", error);
+  });
 
-});
-
-})
-.catch((error) => {
-    console.error('Erro ao conectar no banco de dados:', error);
-})
-
-
-
+export default app;
